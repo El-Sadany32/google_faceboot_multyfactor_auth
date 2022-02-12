5d98b1d8da3f83fc527c873110102d09891dd535
El_Sadany32 <74606586+elsadany32@users.noreply.github.com>
2022-02-12 08:30:58 +0200

    Update Header.js

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -39,11 +39,11 @@ function Header() {
     return (
         <header>
             <div className="logo">
-                <h1><Link to="/">DevAT✮Shop</Link></h1>
+                <h1><Link to="/">Sadoon</Link></h1>
             </div>
 
             <ul style={transForm}>
-                <li><Link to="/"><i className="fas fa-shopping-cart"></i> Cart</Link></li>
+                
                 {
                     isLogged
                     ? userLink()
