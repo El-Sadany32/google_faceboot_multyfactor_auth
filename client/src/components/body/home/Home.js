50a451dfae21b55dc36f49378e7ae06c24a43907
El_Sadany32 <74606586+elsadany32@users.noreply.github.com>
2022-02-12 08:33:42 +0200

    Update Home.js

--- client/src/components/body/home/Home.js
+++ client/src/components/body/home/Home.js
@@ -4,23 +4,13 @@ import './home.css'
 function Home() {
     return (
         <div className="home_page">
-            <h2>Hello everyone!</h2>
+            <h2>Welcome !</h2>
             <p>
-                This site is about user authentication, 
-                so there won't be any other pages here. 
-                If people want to see more about how to 
-                create other websites. You can click on 
-                the link below, visit my youtube channel. 
-                And if you find it useful, please subscribe 
-                for us. Thank you very much!
+               MERN Authentication, Login, Register, Verification email, Facebook, Google, Forget Password
+
             </p>
-            <a href="https://www.youtube.com/c/DevATHTML" target="_blank" 
-            rel="noopener noreferrer">My Youtube</a>
-
-            <h3>Build a simple e-commerce site.</h3>
-
-            <a href="https://youtu.be/uXl77UFkrkQ" target="_blank" 
-            rel="noopener noreferrer">Build a Ecommerce</a>
+            
+          
         </div>
     )
 }
